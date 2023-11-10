af7b742b27b83cc624841068170c27ace350ae77
Soorya B Shetty <101927421+shettysoorya@users.noreply.github.com>
2023-11-10 22:37:54 +0530

    diamondPattern.java

---
+++ diamondPattern.java
@@ -0,0 +1,41 @@
+import java.util.*; 
+  
+public class diamondPattern { 
+  
+    public static void printPattern(int n) 
+    { 
+        int i, j; 
+        int num = 1; 
+        
+        for (i = 1; i <= n; i++) { 
+            
+            for (j = 1; j <= n - i; j++) { 
+                System.out.print(" "); 
+            } 
+            
+            for (j = 1; j <= 2 * i - 1; j++) { 
+                System.out.print("*"); 
+            } 
+            System.out.println(); 
+        } 
+  
+        for (i = n-1; i >= 1; i--) { 
+            
+            for (j = 1; j <= n - i; j++) { 
+                System.out.print(" "); 
+            } 
+            
+            for (j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*"); 
+            } 
+            System.out.println(); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        printPattern(n = sc.nextInt()); 
+    } 
+}
