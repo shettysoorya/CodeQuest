1e0ce21cf436acb55025fa8add0fa36a88d9c48b
Soorya B Shetty <101927421+shettysoorya@users.noreply.github.com>
2023-11-15 23:45:24 +0530

    selectionSort.java

---
+++ selectionSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        // Input array elements
+        int[] arr = new int[n];
+        System.out.print("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sorting the array using Selection Sort
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Displaying the sorted array
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
