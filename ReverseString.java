634bcecbfa6571b9c1de1805a3c8ccdaffcf4863
Soorya B Shetty <101927421+shettysoorya@users.noreply.github.com>
2024-03-03 21:58:27 +0530

    ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
  
-public class RevString {
+public class ReverseString {
     public static void main (String[] args) {
        
         Scanner sc = new Scanner(System.in);
