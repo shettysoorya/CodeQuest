f2c30c499a5b0d980675a22dc58af22dfc84b5a8
Soorya B Shetty <101927421+shettysoorya@users.noreply.github.com>
2024-03-05 23:30:23 +0530

    NthSmall.java

--- NthSmall.java
+++ NthSmall.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class NSmall {
+public class NthSmall {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array size");
